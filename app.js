0b5a46b6675e57666468fadcea7055c95b142678
rogerwillis <roger@rogerwillis.com>
2014-08-23 13:55:45 -0700

    Added functionality
    added input box and some bootstrap stylin'

--- app.js
+++ app.js
@@ -1,45 +1,77 @@
 $(document).ready(function(e) {
-	//Print Number from 1 to 100 to browser
-	
-	for(var i = 1; i<=100; i++)
-	{
-		
-			//if divisible by 15 - Why do I need to put divisible by 15 first for it to work?
-		if(i%15 === 0){
-			
-			$("ul").append("<li>Fizz Bizz</li>");
-			
-			}
-			
-			
-		//if divisibkle by 3
-		else if(i%3 === 0){
-			
-			$("ul").append("<li>Fizz</li>");
-			
-			}
-			
-			//if divisible by 5
-		else if(i%5 === 0){
-			
-			$("ul").append("<li>Bizz</li>");
-			
-			}
-	
-		//if divisible by 3 and 5
-		else {
-			
-			$("ul").append("<li>"+i+"</li>");
-			
-			}
-		}
-	
-    //Check to see if number is divisible by 3 or 5
-	//If not divisible by 3 or 5 just say the number. 
-	
-	//If visible by 3 say fizz 
-	
-	//if divisible by 5 say buzz
-	
-	//If divided by both say fizz buzz
+
+    $(".btn-submit").on('click', function() {
+
+        //reset input
+        $('tr').remove();
+        //get input
+        inputNumber($("#input").val());
+    });
+
+
+    //one named function
+    var inputNumber = function(number) {
+
+        //Prevent Browser Destruction
+        if (+number > 777) {
+
+            $("#myModal").modal("show");
+            $(".modal-body").html("Really? <strong>" + number + "</strong> is a bit too big there sparky! Numbers are cool and all but that is just a bit ridiculous.");
+
+
+        }
+        //check to see if it is a decimal
+        else if (number % 1 != 0) {
+            $("#myModal").modal("show");
+            $(".modal-body").html("Really? <strong>" + number + "</strong> is not a whole number. I only deal in big numbers jerky! Try again!!!!");
+
+        }
+        //is the input blank??
+        else if (number == "") {
+            $("#myModal").modal("show");
+            $(".modal-body").html("Um... Please put in a number there sailor!");
+
+        }
+
+        //is the input blank??
+        else if (number == "0") {
+            $("#myModal").modal("show");
+            $(".modal-body").html("Sorry, Zero is going to output zero items... Nice try there buckaroo!");
+
+        } else {
+            //Loop it baby!
+            for (var i = 1; i <= number; i++) {
+
+                //if divisible by 15 - Why do I need to put divisible by 15 first for it to work?
+                if (i % 15 === 0) {
+                    $(".list").append("<tr class='danger'><td><strong><span class='glyphicon glyphicon-star'></span>   Fizz Buzz</strong></td></tr>");
+                }
+
+                //if divisibkle by 3
+                else if (i % 3 === 0) {
+
+                    $(".list").append("<tr class='warning'><td>Fizz</td></tr>");
+
+                }
+
+                //if divisible by 5
+                else if (i % 5 === 0) {
+
+                    $(".list").append("<tr class='success'><td>Buzz</td></tr>");
+
+                }
+
+                //if divisible by 3 and 5
+                else {
+
+                    $(".list").append("<tr><td>" + i + "</td></tr>");
+
+                }
+            }
+
+        }
+
+
+    };
+
 });
